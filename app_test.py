dad471e24c4fafbfad06f508e4a8d744ed8d1a5f
AydaKSH <145861543+AydaKSH@users.noreply.github.com>
2024-01-13 14:18:15 +0330

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+
+username = 'AydaKSH'
+password = '123456'
+user = st.text_input('username')
+pass_2 = st.text_input('password')
+if user == username:
+  if pass_2 == password:
+    st.markdown('welcome')
+  else:
+    st.markdown('password is wrong')
+else:
+  st.markdown('you are not allowed')
 a = st.number_input('int')
 b = st.number_input('int2')
 
@@ -7,3 +19,4 @@ c = a + b
 st.write('sum is', c)
 st.markdown('this app is private')
 st.markdown('update is available')
+
